ec958d71edad500f0d9b284f268158cb1b694786
duyanh3110 <duyanh19962012@gmail.com>
2024-08-29 11:56:55 +0300

    Update controller name

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const accessController = require("../../controllers/access.controller");
+const AccessController = require("../../controllers/access.controller");
 const asyncHandler = require("../../helpers/asyncHandler");
 const { authentication } = require("../../utils/auth");
 const router = express.Router();
 
 // Sign up
-router.post("/signup", asyncHandler(accessController.signUp));
-router.post("/login", asyncHandler(accessController.login));
+router.post("/signup", asyncHandler(AccessController.signUp));
+router.post("/login", asyncHandler(AccessController.login));
 
 router.use(authentication);
-router.post("/logout", asyncHandler(accessController.logout));
+router.post("/logout", asyncHandler(AccessController.logout));
 
 module.exports = router;
